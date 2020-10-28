b0eed56052c52fe734eec3ed6da0d7716f37a529
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-10-28 21:21:15 +0100

    Initial commit

---
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/ParkirajMeWebServiceApplication.java
@@ -0,0 +1,12 @@
+package hr.fer.littlegreen.parkirajme.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkirajMeWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkirajMeWebServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/hr/fer/littlegreen/parkirajme/webservice/ParkirajMeWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.littlegreen.parkirajme.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkirajMeWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

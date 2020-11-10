7a8d1a5ef3d42e29f00927c8db90a335c9745b87
Dodica <occupied.fall@gmail.com>
2020-11-10 16:08:45 +0100

    Add final modifier to Administrator class

--- src/main/java/hr/fer/littlegreen/parkirajme/webservice/dao/Administrator.java
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/dao/Administrator.java
@@ -1,6 +1,6 @@
 package hr.fer.littlegreen.parkirajme.webservice.dao;
 
-public class Administrator extends User {
+public final class Administrator extends User {
 
     public Administrator(String id, String email, String password, String role, String oib) {
         super(id, email, password, role, oib);

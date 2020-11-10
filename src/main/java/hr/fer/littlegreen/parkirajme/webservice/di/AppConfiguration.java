2f6e7ac2a62c5113db82bcd62f455f434d84f780
Dodica <occupied.fall@gmail.com>
2020-11-10 17:03:33 +0100

    Replace Connect class with a @Bean

--- src/main/java/hr/fer/littlegreen/parkirajme/webservice/di/AppConfiguration.java
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/di/AppConfiguration.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Base64;
 import java.util.HashMap;
 
@@ -21,5 +24,21 @@ public class AppConfiguration {
     @Bean
     public DatabaseManager provideDatabaseManager() {
         return null;
+    }
+
+    @Bean
+    public Connection provideDatabaseConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                "jdbc:postgresql://ec2-54-216-202-161.eu-west-1.compute.amazonaws.com:5432/deg1t0cmdemhn7",
+                "icfjttdivtiins",
+                "447e4ba24d1cc40dff940de459899fbe24bf4c5999da88ff23508aafe16138dc"
+            );
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
 }

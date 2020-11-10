40620dc34b67ca03d8ee6656f6b41030aa59d9ac
Dodica <occupied.fall@gmail.com>
2020-11-10 15:49:13 +0100

    Add session token manager

---
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/session/TokenManager.java
@@ -0,0 +1,9 @@
+package hr.fer.littlegreen.parkirajme.webservice.session;
+
+import org.springframework.lang.Nullable;
+
+public interface TokenManager {
+    void generateToken(String id);
+    @Nullable
+    String checkIfTokenValid(String token);
+}

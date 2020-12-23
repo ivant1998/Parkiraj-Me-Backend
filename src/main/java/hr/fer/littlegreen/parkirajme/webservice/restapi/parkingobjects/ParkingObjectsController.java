793958c5ae862c916fb47fc8ea49c865a7822459
Dodica <occupied.fall@gmail.com>
2020-12-23 03:15:18 +0100

    Implement closest parking objects endpoint

--- src/main/java/hr/fer/littlegreen/parkirajme/webservice/restapi/parkingobjects/ParkingObjectsController.java
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/restapi/parkingobjects/ParkingObjectsController.java
@@ -6,9 +6,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 @RestController
 public class ParkingObjectsController {
@@ -25,10 +32,33 @@ public class ParkingObjectsController {
     @GetMapping("/parkingObjects")
     public ResponseEntity<List<ParkingObject>> parkingObjects() {
         var objects = databaseManager.getParkingObjects();
+        return new ResponseEntity<>(Objects.requireNonNullElseGet(objects, List::of), HttpStatus.OK);
+    }
+
+    @GetMapping("/closestParkingObject?lat=x&long=y")
+    public ResponseEntity<ParkingObject> parkingObjects(
+        @RequestParam BigDecimal latitude,
+        @RequestParam BigDecimal longitude
+    ) {
+        var objects = databaseManager.getParkingObjects();
         if (objects == null) {
-            return new ResponseEntity<>(List.of(), HttpStatus.OK);
+            return new ResponseEntity<>(null, HttpStatus.OK);
         }
-        return new ResponseEntity<>(objects, HttpStatus.OK);
+        ParkingObject min = objects.get(0);
+        var currentPos = sqrt(
+            pow(abs(objects.get(0).getLatitude().doubleValue() - latitude.doubleValue()), 2) + pow(abs(
+                objects.get(0).getLongitude().doubleValue() - longitude.doubleValue()), 2));
+        for (var object : objects) {
+            if (currentPos > sqrt(
+                pow(abs(object.getLatitude().doubleValue() - latitude.doubleValue()), 2) + pow(abs(
+                    object.getLongitude().doubleValue() - longitude.doubleValue()), 2))) {
+                min = object;
+                currentPos = sqrt(
+                    pow(abs(object.getLatitude().doubleValue() - latitude.doubleValue()), 2) + pow(abs(
+                        object.getLongitude().doubleValue() - longitude.doubleValue()), 2));
+            }
+        }
+        return new ResponseEntity<>(min, HttpStatus.OK);
     }
 
 }

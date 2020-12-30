474e99e72eeb16586494ad4cadbc13e989ad18b7
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-12-30 01:11:03 +0100

    add parking object error message

--- src/main/java/hr/fer/littlegreen/parkirajme/webservice/data/database/DatabaseManagerImpl.java
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/data/database/DatabaseManagerImpl.java
@@ -283,6 +283,7 @@ public class DatabaseManagerImpl implements DatabaseManager {
             if (savepoint != null) {
                 try {
                     databaseConnection.rollback(savepoint);
+                    throw new IllegalArgumentException(ErrorMessage.getMessage(e));
                 } catch (SQLException e2) {
                     e2.printStackTrace();
                 }

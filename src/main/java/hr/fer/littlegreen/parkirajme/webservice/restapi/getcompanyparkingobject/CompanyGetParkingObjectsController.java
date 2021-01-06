5f9ffa04a4601fdcbfb1532f8b8f0ef89002b0cd
Dodica <occupied.fall@gmail.com>
2021-01-06 17:51:51 +0100

    Add JSON on responses

--- src/main/java/hr/fer/littlegreen/parkirajme/webservice/restapi/getcompanyparkingobject/CompanyGetParkingObjectsController.java
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/restapi/getcompanyparkingobject/CompanyGetParkingObjectsController.java
@@ -1,7 +1,6 @@
 package hr.fer.littlegreen.parkirajme.webservice.restapi.getcompanyparkingobject;
 
 import hr.fer.littlegreen.parkirajme.webservice.data.database.DatabaseManager;
-import hr.fer.littlegreen.parkirajme.webservice.domain.models.ParkingObject;
 import hr.fer.littlegreen.parkirajme.webservice.domain.session.TokenManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Objects;
 
 @RestController
 public class CompanyGetParkingObjectsController {
@@ -32,7 +28,7 @@ public class CompanyGetParkingObjectsController {
     }
 
     @GetMapping("/company/{companyId}/parkingObjects")
-    public ResponseEntity<List<ParkingObject>> parkingObjects(
+    public ResponseEntity<CompanyGetParkingObjectsResponse> parkingObjects(
         @PathVariable String companyId,
         @RequestHeader("Authentication-Token") String token
     ) {
@@ -41,10 +37,11 @@ public class CompanyGetParkingObjectsController {
         if (companyTokenId != null) {
             if (companyTokenId.equals(companyId)) {
                 var objects = databaseManager.getCompanyParkingObjects(companyId);
-                return new ResponseEntity<>(Objects.requireNonNullElseGet(objects, List::of), HttpStatus.OK);
+                if (objects != null) {
+                    return new ResponseEntity<>(new CompanyGetParkingObjectsResponse(objects), HttpStatus.OK);
+                }
             }
         }
-
         return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
 

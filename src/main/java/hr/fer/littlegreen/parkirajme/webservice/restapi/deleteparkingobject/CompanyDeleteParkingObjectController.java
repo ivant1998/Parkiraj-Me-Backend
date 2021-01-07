ac8ea7f892cf09695c4709ea0aa93bd675b5842b
Dodica <occupied.fall@gmail.com>
2021-01-07 22:01:02 +0100

    Change "Authentication-token" to "Authentication-Token"

--- src/main/java/hr/fer/littlegreen/parkirajme/webservice/restapi/deleteparkingobject/CompanyDeleteParkingObjectController.java
+++ src/main/java/hr/fer/littlegreen/parkirajme/webservice/restapi/deleteparkingobject/CompanyDeleteParkingObjectController.java
@@ -1,21 +1,14 @@
 package hr.fer.littlegreen.parkirajme.webservice.restapi.deleteparkingobject;
 
 import hr.fer.littlegreen.parkirajme.webservice.data.database.DatabaseManager;
-import hr.fer.littlegreen.parkirajme.webservice.data.database.DatabaseManagerImpl;
-import hr.fer.littlegreen.parkirajme.webservice.domain.models.ParkingObject;
 import hr.fer.littlegreen.parkirajme.webservice.domain.session.TokenManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Objects;
 
 @RestController
 public class CompanyDeleteParkingObjectController {
@@ -27,8 +20,8 @@ public class CompanyDeleteParkingObjectController {
     private final TokenManager tokenManager;
 
     public CompanyDeleteParkingObjectController(
-        DatabaseManager databaseManager,
-        TokenManager tokenManager
+        @NonNull DatabaseManager databaseManager,
+        @NonNull TokenManager tokenManager
     ) {
         this.databaseManager = databaseManager;
         this.tokenManager = tokenManager;
@@ -37,7 +30,7 @@ public class CompanyDeleteParkingObjectController {
     @DeleteMapping("/parkingObject/{parkingObjectId}")
     public ResponseEntity<HttpStatus> deleteParkingObject (
         @PathVariable String parkingObjectId,
-        @RequestHeader("Authentication-token") String token
+        @RequestHeader("Authentication-Token") String token
     ) {
         var tokenId = tokenManager.getId(token);
         String companyId = databaseManager.parkingObjectOwner(parkingObjectId);
